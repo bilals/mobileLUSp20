3debe44a9790ca8f373b04c4d80bfb0a8d67ad58
Bilal Said <bilal.said@gmail.com>
2020-05-29 17:27:24 +0300

    AMD Session 6 - RecyclerView (Part2)

--- RecyclerView_45_En/app/src/main/java/com/bilalsaid/recyclerview_45_en/MainActivity.java
+++ RecyclerView_45_En/app/src/main/java/com/bilalsaid/recyclerview_45_en/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView wordListRecyclerView;
     private ArrayList<String> wordsList;
+    private WordListAdapter wordListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +30,11 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             wordsList.add("Word " + (i+1));
         }
-        wordListRecyclerView.setAdapter(new WordListAdapter(this, wordsList));
+        wordListAdapter = new WordListAdapter(this, wordsList);
+        wordListRecyclerView.setAdapter(wordListAdapter);
     }
 
-    private class WordListAdapter extends RecyclerView.Adapter {
+    private class WordListAdapter extends RecyclerView.Adapter<WordViewHolder> {
         private final Context context;
         private final ArrayList<String> wordsList;
         private final LayoutInflater layoutInflater;
@@ -43,18 +47,48 @@ public class MainActivity extends AppCompatActivity {
 
         @NonNull
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null ;//new WordViewHolder(this, itemView);
+        public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View wordItemView = layoutInflater.inflate(R.layout.word_list_item, parent, false);
+            return new WordViewHolder(wordItemView, this);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+            holder.wordItemTextView.setText(wordsList.get(position));
         }
 
         @Override
         public int getItemCount() {
             return wordsList.size();
         }
+    }
+
+    private class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final TextView wordItemTextView;
+        private final WordListAdapter wordListAdapter;
+
+        public WordViewHolder(View wordItemView, WordListAdapter wordListAdapter) {
+            super(wordItemView);
+            wordItemView.setOnClickListener(this);
+            this.wordItemTextView = wordItemView.findViewById(R.id.wordItemTextView);
+            this.wordListAdapter = wordListAdapter;
+        }
+
+        @Override
+        public void onClick(View v) {
+            int wordPosition = getLayoutPosition();
+            String wordAtPosition = wordsList.get(wordPosition);
+            wordsList.set(wordPosition, "Clicked! " + wordAtPosition);
+            wordListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public void addWord(View view) {
+        int wordListSize = wordsList.size();
+        wordsList.add("Wrod " + (wordListSize + 1));
+        // Notify the adapter, that the data has changed.
+        wordListAdapter.notifyItemInserted(wordListSize);
+        // Scroll to the bottom.
+        wordListRecyclerView.smoothScrollToPosition(wordListSize);
     }
 }
